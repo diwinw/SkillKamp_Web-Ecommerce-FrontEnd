d3c4ef5642bc06d936d4be725a2fdf95b2cbca97
DESKTOP-SDQ8ARP\Dew <natchapon428@gmail.com>
2023-04-08 16:15:40 +0700

    [sign up] add process signup

--- src/components/signIn/SignUp.js
+++ src/components/signIn/SignUp.js
@@ -1,19 +1,45 @@
 import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import axios from "axios";
 const SignUp = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(`Name: ${name}, Email: ${email}, Password: ${password}`);
-  };
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   console.log(`Name: ${name}, Email: ${email}, Password: ${password}`);
 
+  // };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = { name, email, password };
+    try {
+      const response = await axios.post(
+        "http://localhost:8844/api/auth/signup",
+        data,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            accept: "*/*",
+          },
+        }
+      );
+      if (response.data == "create user success") {
+        window.location.href = "/";
+      } else {
+        window.alert("Signup failed Please try again");
+      }
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
-      <div class="container text-center">
-        <div class="row">
+      <div className="container text-center">
+        <div className="row">
           <Form onSubmit={handleSubmit}>
             <h2>Sign Up</h2>
             <Form.Group controlId="name">
@@ -29,7 +55,7 @@ const SignUp = () => {
             <Form.Group controlId="email">
               <Form.Label>Email address</Form.Label>
               <Form.Control
-                type="email"
+                // type="email"
                 placeholder="Enter email"
                 value={email}
                 onChange={(event) => setEmail(event.target.value)}
